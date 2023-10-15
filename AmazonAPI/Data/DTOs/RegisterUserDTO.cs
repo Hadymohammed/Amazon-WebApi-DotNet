8f49a85b12e759f9219f70b745e92632a89e7b64
abdelhady <hadymohamed2002@gmail.com>
2023-10-15 20:44:45 +0300

    Add : user role on register

--- AmazonAPI/Data/DTOs/RegisterUserDTO.cs
+++ AmazonAPI/Data/DTOs/RegisterUserDTO.cs
@@ -20,5 +20,7 @@ namespace AmazonAPI.Data.DTOs
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
     }
 }
